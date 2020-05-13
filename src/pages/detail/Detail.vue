b83b7d9853671e6acf8dd49fa6194417d04d037e
nielifa <743739338@qq.com>
2020-05-13 21:40:33 +0800

    detail-header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,20 +1,26 @@
 <template>
-    <div>
-       detail
-       <DetialBanner/>
-    </div>
+  <div>
+    <DetialBanner />
+    <DetailHeader />
+    <div class="content"></div>
+  </div>
 </template>
 
 <script>
-import DetialBanner from './components/Banner'
-    export default {
-        components:{
-            DetialBanner
-        }
-    }
+import DetialBanner from "./components/Banner";
+import DetailHeader from "./components/Header";
+export default {
+  components: {
+    DetialBanner,
+    DetailHeader
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
 @import '~@/assets/styles/varibles.styl';
 
+.content {
+  height: 50rem;
+}
 </style>
